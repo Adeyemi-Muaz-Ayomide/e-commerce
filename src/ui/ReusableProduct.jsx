909d88a47763e4177e3a3a36dca23a1ddb8b05bf
iAmMuadh <adeyemimuaz1@gmail.com>
2023-09-14 18:30:43 +0100

    fixed all bug and refactored ProductList

--- src/ui/ReusableProduct.jsx
+++ src/ui/ReusableProduct.jsx
@@ -2,7 +2,12 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 
-const ReusableProduct = ({ productName, imageUrl }) => {
+const ReusableProduct = ({
+  productName,
+  imageUrl,
+  productPrice,
+  productUsage,
+}) => {
   return (
     <Grid item xs={6} sm={6} md={3}>
       <Card
@@ -24,8 +29,16 @@ const ReusableProduct = ({ productName, imageUrl }) => {
           image={imageUrl}
         />
       </Card>
-      <div>
-        <h1>{productName}</h1>
+      <div className="mt-5">
+        <h3 className=" font-iSerif text-xl font-normal leading-5 tracking-[0.01em] text-[#6b5c4c] max-[479px]:text-lg p-1">
+          {productName}
+        </h3>
+        <h3 className=" font-iSerif text-2xl font-normal leading-5 tracking-[0.01em] text-[#0f172a] max-[479px]:text-lg p-1">
+          {productUsage}
+        </h3>
+        <p className=" font-inter text-sm font-medium leading-5 tracking-[0.01em] text-[#455366] max-[479px]:text-lg p-1">
+          {productPrice}
+        </p>
       </div>
     </Grid>
   );
